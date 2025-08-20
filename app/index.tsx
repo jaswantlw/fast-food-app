0278f17b3ad16658aac5812e9e3b7d94f258a00d
jaswantlw <jaswantlal10@gmail.com>
2025-08-21 01:04:08 +0500

    Setup NativeWind + assets

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+ 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl text-center font-quicksand-bold text-primary">
+        Welcome to React Native Application!
+      </Text>
+    </View>
+  );
+}
