d4990fa477463b85f7cd82104b9ccbb64645e61e
jaswantlw <jaswantlal10@gmail.com>
2025-08-22 16:29:29 +0500

    Setup Appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.APPWRITE_USER_COLLECTION_ID,
+};
