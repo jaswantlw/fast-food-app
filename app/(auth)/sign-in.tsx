30c0df38296fbd5960ae898d9990e5aef27e08fb
jaswantlw <jaswantlal10@gmail.com>
2025-08-22 20:31:35 +0500

    Add Auth Functionality

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { signIn } from "@/lib/appwrite";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
 import { Alert, Text, View } from "react-native";
@@ -9,14 +10,17 @@ const SignIn = () => {
   const [form, setForm] = useState({ email: "", password: "" });
 
   const submit = async () => {
-    if (!form.email || !form.password) {
+    const { email, password } = form;
+
+    if (!email || !password) {
       return Alert.alert("Error", "Please enter credentials");
     }
 
     setIsSubmitting(true);
 
     try {
-      Alert.alert("Success", "User sign in succesfully");
+      await signIn({email, password});
+
       router.push("/");
     } catch (error: any) {
       Alert.alert("Error", error.message);
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { createUser } from "@/lib/appwrite";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
 import { Alert, Text, View } from "react-native";
@@ -9,14 +10,21 @@ const SignUp = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const submit = async () => {
-    if (!form.name || !form.email || !form.password) {
+    const {name, email, password} = form;
+
+    if (!name || !email || !password) {
       return Alert.alert("Error", "Please enter credentials");
     }
 
     setIsSubmitting(true);
 
     try {
-      Alert.alert("Success", "User sign up succesfully");
+      await createUser({
+        email,
+        password,
+        name
+      });
+
       router.push("/");
     } catch (error: any) {
       Alert.alert("Error", error.message);
