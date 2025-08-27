fe42689da0037b2e24dd0086c5766ac64172159b
jaswantlw <jaswantlal10@gmail.com>
2025-08-27 22:50:06 +0500

    Initial implementation of Search screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,13 @@
-import { CreateUserPrams, SignInParams } from "@/type";
-import { Account, Avatars, Client, Databases, ID, Storage } from "react-native-appwrite";
+import { CreateUserPrams, GetMenuParams, SignInParams } from "@/type";
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+  Storage,
+} from "react-native-appwrite";
 
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
@@ -83,6 +91,39 @@ export const logout = async () => {
     return true;
   } catch (error) {
     console.error("Logout error:", error);
+    throw new Error(error as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+    if (category) {
+      queries.push(Query.equal("categories", category));
+    }
+    if (query) {
+      queries.push(Query.search("name", query));
+    }
+
+    const menus = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.menuCollectionId,
+      queries
+    );
+
+    return menus.documents;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getCategories = async ({}) => {
+  try {
+    const categories = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.categoriesCollectionId
+    );
+  } catch (error) {
     throw new Error(error as string);
   }
 };
